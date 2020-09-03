b72b6f829af482a5821b0c769227f0f47fcffa5e
Yewklid <47838762+Yewklid@users.noreply.github.com>
2020-09-02 22:28:15 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    primaryStage.setTitle("TabPane Example");
+    primaryStage.setTitle("Production");
     primaryStage.setScene(new Scene(root));
     primaryStage.show();
   }
