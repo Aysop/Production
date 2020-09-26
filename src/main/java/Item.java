d30eca431004b3e469ddfc9e61f4069a5c3c6ca2
Yewklid <jgmorelli5207@eagle.fgcu.edu>
2020-09-26 00:19:21 -0400

    Beta - Week 6

---
+++ src/main/java/Item.java
@@ -0,0 +1,12 @@
+public interface Item {
+
+  //public int getId();
+  public String getName();
+
+  public void setName(String name);
+
+  public String getManufacturer();
+
+  public void setManufacturer(String manufacturer);
+
+}
